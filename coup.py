e0f92c4a649df11b5a17cb2325529f7727518900
Costas Akrivoulis <costas@cmu.edu>
2018-05-31 19:02:21 -0700

    Refactor main "script" logic into Game class

--- coup.py
+++ coup.py
@@ -4,7 +4,7 @@ import random
 
 def challenge(players, player):
     challenge = False
-    for other in players: 
+    for other in players:
         if player == other:
             continue
 
@@ -48,13 +48,19 @@ class Action:
 class Player:
     def __init__(self, name):
         self.name = name
+        self.money = 2
         self.roles = []
+
+    def alive(self):
+        lives = map(Role.hidden, self.roles)
+        # the player is alive if any role is still hidden
+        return any(lives)
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return "{}: {}".format(self.name, ", ".join(map(str, self.roles)))
+        return "{} ({}): {}".format(self.name, self.money, ", ".join(map(str, self.roles)))
 
 class Role:
     duke = 1
@@ -71,6 +77,9 @@ class Role:
         contessa:   "Contessa",
     }
 
+    def hidden(self):
+        return self.hidden
+
     def __init__(self, role):
         self.hidden = True
         self.role = role
@@ -79,39 +88,84 @@ class Role:
         name = Role.names[self.role]
         return "[" + name + "]" if self.hidden else name
 
-bank = 60
+    def __repr__(self):
+        return self.__str__()
 
-players = [ Player("Costas"), Player("Cam") ]
+class Game:
+    actions = map(Action, [ Action.income, Action.foreign_aid, Action.coup, Action.tax, Action.assassinate, Action.steal, Action.swap ])
 
-game_over = False
+    def __init__(self):
+        self.bank = 60
+        self.game_over = False
+        self.players = []
 
-if not 2 <= len(players) <= 6:
-    print "Invalid number of players ({})! Game only supports between 2-6 players.".format(len(players))
+        self.deck = map(Role, [ Role.duke, Role.assassin, Role.captain, Role.ambassador, Role.contessa ] * 3)
+        random.shuffle(self.deck)
 
-deck = map(Role, [ Role.duke, Role.assassin, Role.captain, Role.ambassador, Role.contessa ] * 3)
-random.shuffle(deck)
-# assign initial roles
+    def add(self, player):
+        if len(self.players) == 5:
+            return False
 
-actions = map(Action, [ Action.income, Action.foreign_aid, Action.coup, Action.tax, Action.assassinate, Action.steal, Action.swap])
+        assert(len(self.deck) > 2)
 
-for player in players:
-    # give each player 2 roles
-    player.roles.extend( [deck.pop(), deck.pop()])
+        player.roles.extend( [self.deck.pop(), self.deck.pop()] )
+        self.players.append(player)
 
-while not game_over:
-    for player in players:
-        print "It's {}'s turn.".format(player)
-        print repr(player)
+        return True
 
-        for action in actions:
-            print "{}: {}".format(action.action, action)
+    def start(self):
+        if not 2 <= len(self.players) <= 6:
+            print "Invalid number of players ({})! Game only supports between 2-6 players.".format(len(self.players))
+            return False
 
-        chosen_action = raw_input("Choose an action: ")
-        if chosen_action == "q" or chosen_action == "quit" or chosen_action == "exit":
-           game_over = True 
-           break
+        print "Welcome to Coup!!"
+        print
 
-        chosen_action = int(chosen_action)
-        
-        if chosen_action not in [ Action.income, Action.foreign_aid, Action.coup ]:
-            challenge = challenge(players, player)
+        print "Here are our players:"
+        for p in self.players:
+            print repr(p)
+        print
+
+        print "Deck:"
+        print self.deck
+        print
+
+        while not self.game_over:
+            if not any(map(Player.alive, self.players)):
+                self.game_over = True
+                continue
+
+            for player in self.players:
+                if not player.alive():
+                    print "Skipping player {} (dead)".format(player)
+                    continue
+
+                print "It's {}'s turn.".format(player)
+                print repr(player)
+                print
+
+                for action in self.actions:
+                    print "{}: {}".format(action.action, action)
+
+                chosen_action = raw_input("Choose an action: ")
+                if chosen_action == "q" or chosen_action == "quit" or chosen_action == "exit":
+                   game_over = True
+                   break
+
+                chosen_action = int(chosen_action)
+
+                if chosen_action not in [ Action.income, Action.foreign_aid, Action.coup ]:
+                    challenge = challenge(players, player)
+
+        return True
+
+def main():
+    players = [ Player("Costas"), Player("Cam") ]
+
+    g = Game()
+    map(g.add, players)
+
+    return g.start()
+
+if __name__ == "__main__":
+    main()
