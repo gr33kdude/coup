86a655b186e02178b9a56ce686ea63acdb0a2c3b
Costas Akrivoulis <costas.akrivoulis@gmail.com>
2019-02-11 10:02:29 -0800

    Work in progress

--- coup.py
+++ coup.py
@@ -1,26 +1,54 @@
 #!/usr/bin/env python
 
 import random
+from math import ceil, log10
+import os
+import enum
+
+def clear_screen():
+    os.system("clear")
+
+# options : [ opts, ... ]
+# question : string
+def choose(options, question):
+    max_digits = int(ceil(log10(len(options)+1)))
+    for i, opt in enumerate(options):
+        print "{:{}}: {}".format(i + 1, max_digits, opt)
+
+    print
+
+    selection = None
+    while selection == None:
+        inp = raw_input(question + "  ")
+
+        # handle invalid input, determine which option
+        try:
+            selection = int(inp) - 1
+            if not 0 <= selection < len(options):
+                raise Exception()
+        except Exception:
+            print "Invalid input, please try again."
+            selection = None
+            continue
+
+    return options[selection]
 
 def challenge(players, player):
-    challenge = False
     for other in players:
         if player == other:
             continue
 
+        challenge_inp = None
         while True:
             challenge_inp = raw_input("{}, would you like to challenge? (y/n) ".format(other))
-            if challenge_inp in [ 'y', 'n' ]:
-                challenge = map(lambda s: {'y': True, 'n': False}[s], challenge_inp)
+            if challenge_inp == 'y':
+                return other
+            elif challenge_inp == 'n':
                 break
-            print "Invalid input"
+            else:
+                print "Invalid input, please try again"
 
-        if challenge:
-            break
-
-    return challenge
-
-class Action:
+class Action(enum.IntEnum):
     income = 1
     foreign_aid = 2
     coup = 3
@@ -29,21 +57,96 @@ class Action:
     steal = 6
     swap = 7
 
-    names = {
-        income:         "Income",
-        foreign_aid:    "Foreign Aid",
-        coup:           "Coup",
-        tax:            "Tax",
-        assassinate:    "Assassinate",
-        steal:          "Steal",
-        swap:           "Swap",
-    }
-
     def __init__(self, action):
         self.action = action
 
+    def can_challenge(self):
+        challengeable = {
+            Action.income:         False,
+            Action.foreign_aid:    True,
+            Action.coup:           False,
+            Action.tax:            True,
+            Action.assassinate:    True,
+            Action.steal:          True,
+            Action.swap:           True,
+        }
+        
+        return challengeable[self.action]
+
+    def requires_target(self):
+        needs_target = {
+            Action.income:         False,
+            Action.foreign_aid:    False,
+            Action.coup:           True,
+            Action.tax:            False,
+            Action.assassinate:    True,
+            Action.steal:          True,
+            Action.swap:           False,
+        }
+
+        return needs_target[self.action]
+
+    # source : Player
+    # target : Player
+    def apply(self, game, source, target):
+        if self == Action.income:
+            source.money += 1
+            game.bank -= 1
+        elif self == Action.foreign_aid:
+            source.money += 2
+            game.bank -= 2
+        elif self == Action.coup:
+            source.money -= 7
+            game.bank += 7
+
+            target.kill()
+        elif self == Action.tax:
+            source.money += 3
+            game.bank -= 3
+        elif self == Action.assassinate:
+            pass
+        elif self == Action.steal:
+            steal_amt = max(target.money, 2)
+            source.money += steal_amt
+            target.money -= steal_amt
+        elif self == Action.swap:
+            pass
+        else:
+            pass
+
+    def valid(self, game, source, target):
+        if source.money >= 10 and self != Action.coup:
+            return False
+
+        if self == Action.income:
+            return game.bank >= 1
+        elif self == Action.foreign_aid:
+            return game.bank >= 2
+        elif self == Action.coup:
+            return source.money >= 7
+        elif self == Action.tax:
+            return game.bank >= 3
+        elif self == Action.assassinate:
+            return target.alive()
+        elif self == Action.steal:
+            return target.money > 0
+        elif self == Action.swap:
+            return True
+
+        return True
+
     def __str__(self):
-        return Action.names[self.action]
+        names = {
+            Action.income:         "Income",
+            Action.foreign_aid:    "Foreign Aid",
+            Action.coup:           "Coup",
+            Action.tax:            "Tax",
+            Action.assassinate:    "Assassinate",
+            Action.steal:          "Steal",
+            Action.swap:           "Swap",
+        }
+        
+        return names[self.action]
 
 class Player:
     def __init__(self, name):
@@ -56,11 +159,26 @@ class Player:
         # the player is alive if any role is still hidden
         return any(lives)
 
+    def kill(self):
+        assert self.alive()
+        lives = filter(Role.hidden, self.roles)
+        if len(lives) > 1:
+            influence = choose(lives, "Which role would you like to reveal?")
+        else:
+            influence = lives[0]
+
+        influence.hide()
+
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return "{} ({}): {}".format(self.name, self.money, ", ".join(map(str, self.roles)))
+        r = "{} ({}): {}".format(self.name, self.money, ", ".join(map(str, self.roles)))
+
+        if not self.alive():
+            r = "~~ {} ~~".format(r)
+
+        return r
 
 class Role:
     duke = 1
@@ -79,6 +197,9 @@ class Role:
 
     def hidden(self):
         return self.hidden
+
+    def hide(self):
+        self.hidden = True
 
     def __init__(self, role):
         self.hidden = True
@@ -102,6 +223,11 @@ class Game:
         self.deck = map(Role, [ Role.duke, Role.assassin, Role.captain, Role.ambassador, Role.contessa ] * 3)
         random.shuffle(self.deck)
 
+    '''
+    def active_players(self):
+        return filter(self
+    '''
+
     def add(self, player):
         if len(self.players) == 5:
             return False
@@ -113,21 +239,33 @@ class Game:
 
         return True
 
+    def prepare_player(self, player):
+        clear_screen()
+
+        print "=== Game State ==="
+        print self
+
+        print "It's {}'s turn.".format(player)
+        print repr(player)
+        print
+
+        for i, action in enumerate(self.actions):
+            print "{}: {}".format(i+1, str(action))
+
+    def play_turn(self, player):
+        pass
+
+
+    # how should this be structured in order to accommodate an AI?
+    # players in a game should be represented using an ID that does not change
+    # 
+
     def start(self):
         if not 2 <= len(self.players) <= 6:
             print "Invalid number of players ({})! Game only supports between 2-6 players.".format(len(self.players))
             return False
 
         print "Welcome to Coup!!"
-        print
-
-        print "Here are our players:"
-        for p in self.players:
-            print repr(p)
-        print
-
-        print "Deck:"
-        print self.deck
         print
 
         while not self.game_over:
@@ -140,27 +278,65 @@ class Game:
                     print "Skipping player {} (dead)".format(player)
                     continue
 
-                print "It's {}'s turn.".format(player)
-                print repr(player)
-                print
+                self.prepare_player(player)
 
-                for action in self.actions:
-                    print "{}: {}".format(action.action, action)
-
-                chosen_action = raw_input("Choose an action: ")
+                # request player action
+                chosen_action = raw_input("Choose an action: ").rstrip("\n")
                 if chosen_action == "q" or chosen_action == "quit" or chosen_action == "exit":
                    game_over = True
                    break
 
-                chosen_action = int(chosen_action)
+                try:
+                    if chosen_action == "":
+                        raise Exception()
 
-                if chosen_action not in [ Action.income, Action.foreign_aid, Action.coup ]:
-                    challenge = challenge(players, player)
+                    action = Action(int(chosen_action))
+                except Exception:
+                    print "Invalid action, please try again"
+                    continue
+
+                print "{} chooses action {}".format(player, action)
+
+                # TODO: manage non-game actions (quit/exit) uniformly?
+                # Should we just have action modify the game state directly if it succeeds?
+                # Meld into same framework?
+
+                target = None
+                if action.requires_target():
+                    valid_players = filter(lambda p: p != player, filter(Player.alive, self.players))
+                    target = choose(valid_players, "Which player will you target?")
+
+                    print target
+
+                if not action.valid(self, player, target):
+                    print "Invalid action, please try again"
+                    continue
+            
+                # resolve challenges if possible
+                challenger = None
+                if action.can_challenge():
+                    challenger = challenge(self.players, player)
+                
+                if challenger != None:
+                    print "{}, {} would like to challenge your action!".format(player, challenger)
+
+                action.apply(self, player, target)
 
         return True
 
+    def __str__(self):
+        s = ""
+
+        s += "Players:\n"
+        for p in self.players:
+            s += "\t{}\n".format(repr(p))
+
+        s += "Deck: {}\n".format(self.deck)
+
+        return s
+
 def main():
-    players = [ Player("Costas"), Player("Cam") ]
+    players = [ Player("Costas"), Player("Cam"), Player("Jeff") ]
 
     g = Game()
     map(g.add, players)
