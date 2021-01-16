21536e86fb640c49209f4ffc541c39bc8ba5810b
Costas Akrivoulis <costas.akrivoulis@gmail.com>
2021-01-16 00:03:03 -0500

    Start C++ version with idea comments

---
+++ coup.cpp
@@ -0,0 +1,13 @@
+/*
+ * Coup game design
+ *
+ * - Have some RAM data structures that could be persisted to disk
+ * - Server that starts and maintains games
+ * - Allows players to join/leave/spectate/pause
+ * - Players can submit actions as clients
+ *
+ *
+ *
+ * - Game state
+ * - Player state
+ */
